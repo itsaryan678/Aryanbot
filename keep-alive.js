620ea98f68774abb3146ccfa3bb987fcd1ed99ed
Itz Aryan <163823860+itsaryan678@users.noreply.github.com>
2024-12-07 19:05:55 +0530

    Update keep-alive.js

--- keep-alive.js
+++ keep-alive.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 8080
 
 // const port = 8080
 
-app.get('/', (req, res) => res.send('Remade By Priyansh!!'));
+app.get('/', (req, res) => res.send('Remade By Aryan Chauhan!!'));
 
 app.listen(port, () =>
 	console.log(`Your app is listening a http://localhost:${port}`)
